1911dd5b3bfe607bb82aec7490bba98cd11feb97
Daniel Montenegro <dannielmdr7@gmail.com>
2025-12-31 16:06:06 -0500

    feature:se ajustan todos los providers a trabajar con data mock

--- src/modules/cart/adapters/output/repository.provider.ts
+++ src/modules/cart/adapters/output/repository.provider.ts
@@ -1,14 +1,12 @@
-import {
-  providerFactory,
-  EnvironmentEnum,
-} from '@common/utils/provider-factory';
 import type { CartOutputRepositoryInterface } from '../../domain/ports/cart.output-repository.interface';
 import { CartRepositoryMock } from './cart.repository.mock';
-import { CartHttpRepository } from './cart-http.repository';
 
-export const cartRepository: CartOutputRepositoryInterface = providerFactory({
-  [EnvironmentEnum.Mocked]: new CartRepositoryMock(),
-  [EnvironmentEnum.Local]: new CartHttpRepository(),
-  [EnvironmentEnum.Development]: new CartHttpRepository(),
-  [EnvironmentEnum.Production]: new CartHttpRepository(),
-});
+export const cartRepository: CartOutputRepositoryInterface =
+  new CartRepositoryMock();
+
+// export const cartRepository: CartOutputRepositoryInterface = providerFactory({
+//   [EnvironmentEnum.Mocked]: new CartRepositoryMock(),
+//   [EnvironmentEnum.Local]: new CartHttpRepository(),
+//   [EnvironmentEnum.Development]: new CartHttpRepository(),
+//   [EnvironmentEnum.Production]: new CartHttpRepository(),
+// });
--- src/modules/landing/adapters/output/repository.provider.ts
+++ src/modules/landing/adapters/output/repository.provider.ts
@@ -1,15 +1,13 @@
-import {
-  providerFactory,
-  EnvironmentEnum,
-} from '@common/utils/provider-factory';
 import type { LandingOutputRepositoryInterface } from '../../domain/ports/landing.output-repository.interface';
 import { LandingRepositoryMock } from './landing.repository.mock';
-import { LandingHttpRepository } from './landing-http.repository';
 
 export const landingRepository: LandingOutputRepositoryInterface =
-  providerFactory({
-    [EnvironmentEnum.Mocked]: new LandingRepositoryMock(),
-    [EnvironmentEnum.Local]: new LandingHttpRepository(),
-    [EnvironmentEnum.Development]: new LandingHttpRepository(),
-    [EnvironmentEnum.Production]: new LandingHttpRepository(),
-  });
+  new LandingRepositoryMock();
+
+// export const landingRepository: LandingOutputRepositoryInterface =
+//   providerFactory({
+//     [EnvironmentEnum.Mocked]: new LandingRepositoryMock(),
+//     [EnvironmentEnum.Local]: new LandingHttpRepository(),
+//     [EnvironmentEnum.Development]: new LandingHttpRepository(),
+//     [EnvironmentEnum.Production]: new LandingHttpRepository(),
+//   });
